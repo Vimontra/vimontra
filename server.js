53036ca367927a8f351c7f80521bb9142db868e9
tonggii <chachaco@live.com>
2015-07-20 23:55:31 +0800

    vimontra add pic

--- server.js
+++ server.js
@@ -2,3 +2,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(4000);
+
+console.log("start server at http://localhost:4000")
