120230ab9e9b7bfcbac5bd14999f21f88a3e5bb3
tonggii <chachaco@live.com>
2015-07-21 00:09:58 +0800

    updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(4000);
+connect().use(serveStatic(__dirname)).listen(8000);
 
-console.log("start server at http://localhost:4000")
+console.log("start server at http://localhost:8000")
