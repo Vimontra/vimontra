8406cd432af77a8a4125b49c0e327b42afd62a68
tonggii <chachaco@live.com>
2015-08-17 18:04:49 +0800

    vimontra new

--- js/scripts.js
+++ js/scripts.js
@@ -63,33 +63,5 @@ $('.panel-thumbnail>a').click(function(e){
 });
 
 
-/* google maps */
-$(document).ready(function(){/* google maps -----------------------------------------------------*/
-  google.maps.event.addDomListener(window, 'load', initialize);
-
-  function initialize() {
-
-    /* position Amsterdam */
-    var latlng = new google.maps.LatLng(13.696258, 100.538693);
-
-    var mapOptions = {
-      center: latlng,
-      scrollWheel: false,
-      zoom: 13
-    };
-
-    var marker = new google.maps.Marker({
-      position: latlng,
-      url: '/',
-      animation: google.maps.Animation.DROP
-    });
-
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    marker.setMap(map);
-
-  };
-/* end google maps -----------------------------------------------------*/
-});
-
 
 });
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8000);
+connect().use(serveStatic(__dirname)).listen(8001);
 
-console.log("start server at http://localhost:8000")
+console.log("start server at http://localhost:8001")
